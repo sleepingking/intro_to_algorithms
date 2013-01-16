6178e5bff23f2997f7a71fc2e34c1d2f84760053
Ben Chen <ben.chen@me.com>
2013-01-15 23:21:48 -0800

    Initial Commit

---
+++ Introduction to Algorithm/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Introduction to Algorithm
+//
+//  Created by Ben Chen on 1/15/13.
+//  Copyright (c) 2013 Sleeping Heroes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
